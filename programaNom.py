6ac6684b543275bf912687337e97e9eea3ff6c8b
nadiagarciamacias <nadiagarcia@iesmontsia.org>
2024-01-08 21:50:01 +0100

    programa per aprendre metodes

---
+++ programaNom.py
@@ -0,0 +1,17 @@
+"""Classe per crear un programa que te diu el nom"""
+class programa: 
+
+    def crear_nom(nom, cognoms):
+        """ Concatenar nom i cognoms """
+        nom_complet = nom + " " + cognoms
+        return nom_complet
+        
+    def saludar(quiEts): 
+        """ Saludar en català """
+        salutacio = f"Hola, {quiEts}, que tinguis un bon dia."
+        return salutacio
+
+
+elMeuNom = programa.crear_nom("maria", "merino")
+programa.saludar(elMeuNom)
+print(elMeuNom)
